2e317cdfbddb05667d6436602d178fc01dcb5619
Damyan Kalev <dzkalev@gmail.com>
2017-04-01 22:00:49 +0100

    firebase

--- server/index.js
+++ server/index.js
@@ -4,8 +4,13 @@ const app = express()
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-res.render('index', { title: 'Trash' });
-})
+	localStorage.setItem("dean", "bean");
+	res.render('index');
+});
+
+app.post('/data', (req, res) => {
+	localStorage.setItem("dean", "bean");
+});
 
 app.listen(3000, () => console.log('Server running on port 3000'))
 
