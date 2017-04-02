38e061a38a42312ac91406e7a5e40fa5236de9de
Damyan Kalev <dzkalev@gmail.com>
2017-04-02 03:04:18 +0100

    bins

--- server/index.js
+++ server/index.js
@@ -4,12 +4,11 @@ const app = express()
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-	localStorage.setItem("dean", "bean");
 	res.render('index');
 });
 
 app.post('/data', (req, res) => {
-	localStorage.setItem("dean", "bean");
+
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'))
